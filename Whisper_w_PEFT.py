053298c82b15c05c443200b38f9b8ae76063f71e
sutungpo <sutungpo@qq.com>
2024-08-28 20:11:11 +0800

    3rd

--- Whisper_w_PEFT.py
+++ Whisper_w_PEFT.py
@@ -102,8 +102,8 @@ training_args = Seq2SeqTrainingArguments(
     eval_steps=500,
     remove_unused_columns=False,  # required as the PeftModel forward doesn't have the signature of the wrapped model's forward
     label_names=["labels"],  # same reason as above
-    ddp_find_unused_parameters=False,
-    dataloader_pin_memory=False,
+    gradient_checkpointing=True,
+    gradient_checkpointing_kwargs = {"use_reentrant": False}, #must be false for DDP
 )
 
 # This callback helps to save only the adapter weights and remove the base model weights.

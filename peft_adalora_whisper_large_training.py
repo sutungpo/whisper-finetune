fdac10afbcaf8713607ac1af79a40861889fa39c
sutungpo <sutungpo@qq.com>
2024-09-22 16:07:36 +0800

    scaler

--- peft_adalora_whisper_large_training.py
+++ peft_adalora_whisper_large_training.py
@@ -663,6 +663,7 @@ def main():
     logger.info(f"  Total train batch size (w. parallel, distributed & accumulation) = {total_batch_size}")
     logger.info(f"  Gradient Accumulation steps = {args.gradient_accumulation_steps}")
     logger.info(f"  Total optimization steps = {args.max_train_steps}")
+    logger.info(f"  accelerator num_processes = {accelerator.num_processes}")
     # Only show the progress bar once on each machine.
     progress_bar = tqdm(range(args.max_train_steps), disable=not accelerator.is_local_main_process)
     global_step = 0
